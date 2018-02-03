cd777ef852175b43d861402749b1182aeea49b50
joeyguo <lgsincy@gmail.com>
2018-02-03 16:22:36 +0800

    support mulit folder

--- index.js
+++ index.js
@@ -9,5 +9,23 @@ module.exports = function (data, opts, tool) {
     
     tool.writeFile("gka.css", css(data, prefix, frameduration));
     tool.writeFile("gka.html", html(names, prefix));
+
+    // 子目录中每一个文件夹都生成对应html、css
+    var frames = data.frames,
+        keyMap = data.keyMap,
+        cssFileName = '';
+
+    if (!keyMap || Object.keys(keyMap).length === 1) return;
+
+    for (var key in keyMap) {
+        cssFileName = key + "-gka.css";
+    	tool.writeFile(cssFileName, css({
+    		frames: data.frames,
+    		keyMap: {
+    			[key] : keyMap[key]
+    		}
+    	}, prefix, frameduration));
+    	tool.writeFile(key + "-gka.html", html(names, prefix, cssFileName));
+    }
 };
 
