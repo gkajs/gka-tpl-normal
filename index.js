f758999eaf2ad60ff598ace25bd2ab58c72ba0f2
joeyguo <lgsincy@gmail.com>
2018-02-09 13:57:16 +0800

    1.2.0

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-var html = require("./lib/html"),
-	css = require("./lib/css");
-
-module.exports = function (data, opts, tool) {
-    var prefix = opts.prefix,
-        frameduration = opts.frameduration;
-
-    var names = tool.getNames();
+var gkaUtils = require('gka-utils'),
+    writeSync = gkaUtils.file.writeSync;
     
-    tool.writeFile("gka.css", css(data, prefix, frameduration));
-    tool.writeFile("gka.html", html(names, prefix));
+var html = require("./lib/html"),
+    css = require("./lib/css");
 
-    // 子目录中每一个文件夹都生成对应html、css
-    var frames = data.frames,
-        keyMap = data.keyMap,
-        cssFileName = '';
+module.exports = function (data, opts, cb) {
+    
+    var dir = opts.imageDir;
 
-    if (!keyMap || Object.keys(keyMap).length === 1) return;
+    function run(data, opts, key) {
+        var name = (key? key + '-' : '') + 'gka',
+            cssName = name + '.css',
+            htmlName = name + '.html';
 
-    for (var key in keyMap) {
-        cssFileName = key + "-gka.css";
-    	tool.writeFile(cssFileName, css({
-    		frames: data.frames,
-    		keyMap: {
-    			[key] : keyMap[key]
-    		}
-    	}, prefix, frameduration));
-    	tool.writeFile(key + "-gka.html", html(names, prefix, cssFileName));
+        writeSync([dir, '..', cssName], css(data, opts));
+        writeSync([dir, '..', htmlName], html(data, opts, cssName));
     }
-};
 
+    run(data, opts);
+
+    // 对每个子目录都进行处理
+    gkaUtils._.effectSubFolderSync(run, data, opts);
+
+    cb && cb();
+}
